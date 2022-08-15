1d8ecd328d902cb4acffbb67a2e84adf74f126f5
AVI TOMAR <78687121+tomaravi7@users.noreply.github.com>
2022-08-15 22:13:29 +0530

    intitial commit

---
+++ assets/js/preloader.js
@@ -0,0 +1,13 @@
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+    document.querySelector("#preload").style.visibility = "visible";
+    window.scrollTo(0, 0);
+  } else {
+    const myTimeout = setTimeout(loadpage, 4200);
+  }
+};
+function loadpage() {
+  document.querySelector("#preload").style.display = "none";
+  document.querySelector("body").style.visibility = "visible";
+}
