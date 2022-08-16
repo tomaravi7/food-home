35a80b36d4aae744f257a1c37c8aea33ca5817b1
AVI TOMAR <78687121+tomaravi7@users.noreply.github.com>
2022-08-17 01:16:24 +0530

    small update to navbar

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,13 +1,15 @@
 document.onreadystatechange = function () {
   if (document.readyState !== "complete") {
     document.querySelector("body").style.visibility = "hidden";
+    document.querySelector("body").style.overflowY="hidden";
     document.querySelector("#preload").style.visibility = "visible";
     window.scrollTo(0, 0);
   } else {
-    const myTimeout = setTimeout(loadpage, 4200);
+    const myTimeout = setTimeout(loadpage, 200);
   }
 };
 function loadpage() {
   document.querySelector("#preload").style.display = "none";
   document.querySelector("body").style.visibility = "visible";
+  document.querySelector("body").style.overflowY="scroll";
 }
